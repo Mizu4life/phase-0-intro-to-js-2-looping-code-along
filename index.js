2e48572b2408ef47ffd05d3784032711b51ad8b8
Zainab Al-Najjar <eng.zainab.alnajar@gmail.com>
2024-02-15 19:50:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names , type){
+    let arr=[];
+    for(let i=0; i< names.length;i++){
+        arr.push("Thank you, " + names[i] + ", for the wonderful " + type + " gift!");
+    }
+    return arr;
+};
+// console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+function countDown(countdown){
+    while (countdown >= 0) {
+        console.log(countdown--);
+      }
+}
+countDown(10);
